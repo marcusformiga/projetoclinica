d07527fceccf4bb291223f817ced1b59457cf8e9
marcusformiga <kiko.antares@gmail.com>
2025-03-19 16:02:21 -0300

    chore: primeiro commit e add primeiras dependencias do projeto

---
+++ src/main/java/com/poc/pocjava/PocjavaApplication.java
@@ -0,0 +1,13 @@
+package com.poc.pocjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/pocjava/PocjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.pocjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
